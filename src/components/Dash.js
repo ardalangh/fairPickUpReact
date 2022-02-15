80c5f3dc026bfad1879b331bdc979d739c80f4b2
ardalangh <rdghoorchian@gmail.com>
2022-02-15 14:09:16 -0800

    dash component added for ali to complete

---
+++ src/components/Dash.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const my_events = [
+	{
+		'name': 'Nima\'s Soccer',
+		'datetime': '02/15/2022 5:30 PM',
+		'Number of players attending': 20,
+	},
+	{
+		'name': 'Nima\'s Soccer',
+		'datetime': '02/15/2022 5:30 PM',
+		'Number of players attending': 20,
+	},
+	{
+		'name': 'Nima\'s Soccer',
+		'datetime': '02/15/2022 5:30 PM',
+		'Number of players attending': 20,
+	},
+];
+
+function Dash(props) {
+	return (
+		<div></div>
+	);
+}
+
+export default Dash;
